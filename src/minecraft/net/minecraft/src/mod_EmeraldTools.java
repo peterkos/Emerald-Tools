62ea6ef10e485b4d8d517542a81d44c73833dd33
Peter <pkos99@gmail.com>
2013-03-26 10:56:35 -0400

    1.5 :D

---
+++ src/minecraft/net/minecraft/src/mod_EmeraldTools.java
@@ -0,0 +1,108 @@
+package net.minecraft.src;
+import java.util.Random;
+
+public  class mod_EmeraldTools extends BaseMod {
+    public static final Item emeraldpickaxe = new ItemPickaxe (20000, EnumToolMaterial.EMP).setUnlocalizedName("empk");
+    public static final Item emeraldsword = new ItemSword (20001, EnumToolMaterial.EMP).setUnlocalizedName("emsw");
+    public static final Item emeraldaxe = new ItemAxe (20002, EnumToolMaterial.EMP).setUnlocalizedName("emax");
+    public static final Item emeraldshovel = new ItemHoe (20003, EnumToolMaterial.EMP).setUnlocalizedName("emsh");
+    public static final Item emeraldhoe = new ItemHoe (20004, EnumToolMaterial.EMP).setUnlocalizedName("emho");
+   //Armor
+  	public static final Item EmeraldHelmet = (new ItemArmor(20005,EnumArmorMaterial.EMA ,5,0 )).setUnlocalizedName("emhe");
+  	public static final Item EmeraldChestplate = (new ItemArmor(20006, EnumArmorMaterial.EMA ,5,1 )).setUnlocalizedName("emch");
+  	public static final Item EmeraldPants = (new ItemArmor(20007,EnumArmorMaterial.EMA ,5,2 )).setUnlocalizedName("empa");
+  	public static final Item EmeraldBoots = (new ItemArmor(20008,EnumArmorMaterial.EMA, 5, 3 )).setUnlocalizedName("embo");
+    
+  	//ACHIVEMENTS
+  	
+  	public static Achievement emeralds = new Achievement(1001, "mineem", -2, 3, Item.emerald, AchievementList.diamonds).registerAchievement();
+	public static Achievement emeraldpick = new Achievement(1000, "craftem", -2, 1, mod_EmeraldTools.emeraldpickaxe, mod_EmeraldTools.emeralds).setSpecial().registerAchievement();
+	
+	//Mining Emeralds
+	public void onItemPickup(EntityPlayer entityplayer, ItemStack itemstack)
+	{
+		if(itemstack.itemID == Item.emerald.itemID){
+			entityplayer.addStat(emeralds, 1);
+		}
+	}
+	
+	//Crafting an Emerald Pick
+	public void takenFromCrafting(EntityPlayer entityplayer, ItemStack itemstack, IInventory iinventory)
+	{
+		if(itemstack.itemID == mod_EmeraldTools.emeraldpickaxe.itemID){
+			entityplayer.addStat(emeraldpick, 1);
+		}
+	}
+  	
+    public void load()
+    {
+    	
+    	ModLoader.addAchievementDesc(emeralds, "We need to get greener", "Acquire Emeralds!");
+		ModLoader.addAchievementDesc(emeraldpick, "High Miner", "Craft an Emerald Pickaxe");
+    	
+    	//EmeraldPickaxe
+        ModLoader.addName(emeraldpickaxe, "Emerald Pickaxe");
+        ModLoader.addRecipe(new ItemStack(emeraldpickaxe, 1), new Object[]{
+        "***", " $ ", " $ ", Character.valueOf('*'), Item.emerald, Character.valueOf('$'), Item.stick});
+        
+        //EmeraldSword
+        ModLoader.addName(emeraldsword, "Emerald Sword");
+        ModLoader.addRecipe(new ItemStack(emeraldsword, 1), new Object[]{
+        " * ", " * ", " $ ", Character.valueOf('*'), Item.emerald, Character.valueOf('$'), Item.stick});
+        
+        //EmeraldAxe (craftingrecipe 1)
+        ModLoader.addName(emeraldaxe, "Emerald Axe");
+        ModLoader.addRecipe(new ItemStack(emeraldaxe, 1), new Object[]{
+        " **", " $*", " $ ", Character.valueOf('*'), Item.emerald, Character.valueOf('$'), Item.stick});
+        //EmeraldAxe (craftingrecipe 2)
+        ModLoader.addName(emeraldaxe, "Emerald Axe");
+        ModLoader.addRecipe(new ItemStack(emeraldaxe, 1), new Object[]{
+        "** ", "*$ ", " $ ", Character.valueOf('*'), Item.emerald, Character.valueOf('$'), Item.stick});
+        
+        //EmeraldShovel
+        ModLoader.addName(emeraldshovel, "Emerald Shovel");
+        ModLoader.addRecipe(new ItemStack(emeraldshovel, 1), new Object[]{
+        " * ", " $ ", " $ ", Character.valueOf('*'), Item.emerald, Character.valueOf('$'), Item.stick});
+        
+        ModLoader.addName(emeraldhoe, "Emerald Hoe");
+        ModLoader.addRecipe(new ItemStack(emeraldhoe, 1), new Object[]{
+        "** ", " $ ", " $ ", Character.valueOf('*'), Item.emerald, Character.valueOf('$'), Item.stick});
+        
+        ModLoader.addName(emeraldhoe, "Emerald Hoe");
+        ModLoader.addRecipe(new ItemStack(emeraldhoe, 1), new Object[]{
+        " **", " $ ", " $ ", Character.valueOf('*'), Item.emerald, Character.valueOf('$'), Item.stick});
+      //===ARMOR====  
+        		// Emerald Helmet Armor
+     			ModLoader.addName(EmeraldHelmet, "Emerald Helmet");
+     			ModLoader.addRecipe(new ItemStack(EmeraldHelmet,1), new Object[]{
+     				"***", "* *", "   ", Character.valueOf('*'), Item.emerald});
+     			
+     			// Emerald Chest Armor
+     			ModLoader.addName(EmeraldChestplate, "Emerald Chestplate");
+     			ModLoader.addRecipe(new ItemStack(EmeraldChestplate,1), new Object[]{
+     				"* *", "***", "***", Character.valueOf('*'), Item.emerald});
+     			
+     			
+     			// Emerald Pants Armor
+     			ModLoader.addName(EmeraldPants, "Emerald Pants");
+     			ModLoader.addRecipe(new ItemStack(EmeraldPants,1), new Object[]{
+     				"***", "* *", "* *", Character.valueOf('*'), Item.emerald});
+     			
+     			// Emerald Boots Armor
+     			ModLoader.addName(EmeraldBoots, "Emerald Boots");
+     			ModLoader.addRecipe(new ItemStack(EmeraldBoots,1), new Object[]{
+     				"   ", "* *", "* *", Character.valueOf('*'), Item.emerald});
+     				
+     			ModLoader.addArmor("emar"); 
+
+    }
+
+
+
+    public String getVersion()
+    {
+        return "1.0";
+    }
+
+
+}
---
+++ temp/src/minecraft/net/minecraft/src/Location.java
@@ -0,0 +1,25 @@
+package net.minecraft.src;
+
+import argo.jdom.JsonNode;
+import net.minecraft.src.ValueObject;
+
+public class Location extends ValueObject {
+
+   public String field_96396_a;
+   public String field_96395_b;
+
+
+   public static Location func_98167_a(JsonNode p_98167_0_) {
+      Location var1 = new Location();
+      var1.field_96396_a = p_98167_0_.getStringValue(new Object[]{"locationId"});
+      var1.field_96395_b = p_98167_0_.getStringValue(new Object[]{"locationName"});
+      return var1;
+   }
+
+   public static Location func_98168_a(JsonNode p_98168_0_, String p_98168_1_) {
+      Location var2 = new Location();
+      var2.field_96396_a = p_98168_0_.getStringValue(new Object[]{p_98168_1_, "locationId"});
+      var2.field_96395_b = p_98168_0_.getStringValue(new Object[]{p_98168_1_, "locationName"});
+      return var2;
+   }
+}
